b0e5f1979c4c9bd7cfdfc9cbc84e5c5b7e62b4d4
Miles Weber <vorvon4@gmail.com>
2021-09-01 01:13:18 -0400

    Initial Setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include "layouts/_document_head.php" ?>
+<body>
+    <h1 class="text-center"> Password Keeper</h1>
+    <div class="container text-center w-25 border bg-light">
+        <h5>Please Log in or Register to continue</h5>
+        <hr>
+        <button class="btn btn-primary" onclick="window.location.href='pages/login_page.php'">Login</button>
+        <br>-or-<br>
+        <button class="btn btn-primary" onclick="window.location.href='pages/register_page.php'">Register</button>
+        <br><br>
+    </div>
+</body>
