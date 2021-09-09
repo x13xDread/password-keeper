70f2d42260df8bc5007ab88fc73644745c40fc42
Miles Weber <vorvon4@gmail.com>
2021-09-09 13:48:13 -0400

    Created Navbar as a layout

---
+++ layouts/_navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../pages/main.php">Password Keeper</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">View Passwords</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add Password</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../scripts/logout_handler.php">Logout</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            User Settings
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Change Password</a></li>
+            <li><a class="dropdown-item" href="#">Give Access to another User</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Delete Account</a></li>
+          </ul>
+        </li>        
+      </ul>
+    </div>
+  </div>
+</nav>
--- pages/main.php
+++ pages/main.php
@@ -1,13 +1,11 @@
 <?php include "../layouts/_document_head.php"?>
 <?php include "../scripts/session_handler.php"?>
+<?php include "../layouts/_navbar.php" ?>
 <div class="container">
     <!--where the table display is going to go-->
     <div class="container border w-50 text-center mt-5 p-2 bg-light">
         <h1>Password Keeper</h1>
-        <h5>Passwords</h5>
-        <!--logout button-->
-        <form action="../scripts/logout_handler.php" method="post">
-            <button type="submit" class="btn btn-primary">Logout</button>
-        </form>
+        <h3>Welcome <?php echo $_SESSION['username']?></h3>
+        
     </div>
 </div>
