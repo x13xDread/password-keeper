c9078725fa2ea8d580ecca83a279c7d2d717464d
Miles Weber <vorvon4@gmail.com>
2021-09-14 12:18:56 -0400

    Added Data Tables Library

--- layouts/_document_head.php
+++ layouts/_document_head.php
@@ -9,4 +9,9 @@
     <!--Include Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    <!--Include Data Tables-->
+    <link href="cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src ="cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    
 </head>
