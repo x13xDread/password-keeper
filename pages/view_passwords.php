1baeeaceacf3cf0a56c9ae58d262ab1064035b4d
Miles Weber <vorvon4@gmail.com>
2021-09-14 12:19:28 -0400

    Added view_passwords under pages

---
+++ pages/view_passwords.php
@@ -0,0 +1,13 @@
+<?php include "../layouts/_document_head.php"?>
+<?php include "../scripts/session_handler.php"?>
+<?php include "../layouts/_navbar.php" ?>
+
+<!--page container-->
+<div class="container">
+    <!--page content-->
+    <div class="container mt-3 bg-light border">
+        <h1 class = "text-center">Accounts for <?php echo " " . $_SESSION['username']; ?></h1>
+        <!--data table-->
+
+    </div>
+</div>
